76755c4108ec0bd8b15c504ce953faccb7f29f93
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-03-20 15:36:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -38,6 +38,8 @@ if(!isset($_SESSION['access_token'])){
 	<head> 
 		<title> API </title>
 		<link rel="stylesheet" type="text/css" href="css/styles_index.css">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	</head>
 	<body>
 		<div class="header">
