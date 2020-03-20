5ca27291846aea321c1c7e45a8b17fa0caf2fe21
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-03-20 15:25:26 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$google_client = new Google_Client();
+
+$google_client->setClientId('910542507778-2hij5mionnfnmstepbh692b9dse51fs4.apps.googleusercontent.com');
+
+$google_client->setClientSecret('hQ-03XkGGaxyPPXZCOqdt8Ae');
+
+$google_client->setRedirectUri('https://kyzier.herokuapp.com/');
+
+$google_client->addScope('email');
+
+$google_client->addScope('profile');
+
+session_start();
+
+?>
